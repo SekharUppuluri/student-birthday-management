22484d033424a4b79014a7074435f626e5972155
sekharuppuluri <sekharuppuluri3@gmail.com>
2025-10-11 20:12:10 +0530

    ✨ Added register_student function to improve student record addition process

--- modules/student_manager.py
+++ modules/student_manager.py
@@ -2,3 +2,11 @@
 Manage student data Operations using Google Sheets as backend
 """
 from services.sheet_service import fetch_all_students, add_student_record, update_student_record
+
+def register_student(student):
+    """ Adding a new student record to Google Sheet """
+    success = add_student_record(student)
+    if success :
+        print(f" student {student['Name']} added successfully ")
+    else:
+        print("Failed to add student")
