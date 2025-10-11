85effbede5388301657ab20ebcd07ac44b6264c0
sekharuppuluri <sekharuppuluri3@gmail.com>
2025-10-11 19:37:40 +0530

    ✨ Added get_worksheet function

--- services/sheet_service.py
+++ services/sheet_service.py
@@ -1,4 +1,16 @@
+"""
+Handles all Google Sheets read/write operations for the Student Birthday Management System
+"""
 import gspread
 from google.auth.transport.requests import Request
 from google.oauth2.service_account import Credentials
 from .google_client import get_gspread_client
+
+SPREADSHEET_NAME = "Student Data"
+
+def get_worksheet(sheet_name = "sheet1"):
+    """ Open the Google Sheet and return the specified worksheet """
+    client = get_gspread_client()
+    spreadsheet = client.open(SPREADSHEET_NAME)
+    worksheet = spreadsheet.worksheet(sheet_name)
+    return worksheet
