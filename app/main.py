739b607ceaf17fcdd662e001d3bb54705e2f0b47
sekharuppuluri <sekharuppuluri3@gmail.com>
2025-10-13 08:43:42 +0530

    Updated menu options and imported edit_student function for enhanced student management features For APP

--- app/main.py
+++ app/main.py
@@ -1,16 +1,15 @@
 import streamlit as st
-
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-from modules.student_manager import register_student, view_all_students, search_student
+from modules.student_manager import register_student, view_all_students, search_student, edit_student 
  
 
 st.set_page_config(page_title= "Student Birthday Management", layout="wide")
 st.title("🎓 Student Birthday Management System")
 
-menu = ["Home","Register Student","View Students","Search Student" ]
+menu = ["Home","Register Student","View Students","Search Student","Update Student"]
 choice = st.sidebar.selectbox("Menu", menu)
 
 if choice == "Home":
@@ -41,8 +40,3 @@ elif choice == "Register Student":
 elif choice == "View Students":
     st.subheader("All Students")
     view_all_students()
-elif choice == "Search Student":
-    st.subheader("Search Student by Roll NUmber")
-    roll_no = st.text_input("Enter ROll Number")
-    if st.button("Search"):
-        search_student(roll_no)
