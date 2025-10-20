8a03e636f6b6bf9458f910f69b46e3a23f55303c
sekharuppuluri <sekharuppuluri3@gmail.com>
2025-10-20 10:04:27 +0530

    Updated register_student function to use Streamlit for success and error messages

--- modules/student_manager.py
+++ modules/student_manager.py
@@ -9,11 +9,11 @@ from services.sheet_service import fetch_all_students, add_student_record, updat
 def register_student(student):
     """ Adding a new student record to Google Sheet """
     success = add_student_record(student)
-    if success :
-        print(f" student {student['Name']} added successfully ")
+    if success:
+        st.success(f"🎉 Student '{student['Name']}' added successfully!")
     else:
-        print("Failed to add student")
-
+        st.error("⚠️ Failed to add student. Please try again.")
+        
 
 def view_all_students():
     """ Fetch and display all students """
